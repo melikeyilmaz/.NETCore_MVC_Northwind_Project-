5b57c208789895383962508f200248134d83a6fd
melikeyilmaz <47854574+melikeyilmaz@users.noreply.github.com>
2023-09-16 17:25:43 +0300

    Add project files.

---
+++ Odev_1_MelikeYilmaz/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Odev_1_MelikeYilmaz.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
